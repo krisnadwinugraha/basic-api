c43cf81599e30448545c4e734d07b596ef7dabdc
Krisna Dwi Nugraha <krisnadwinugrhaabe@gmail.com>
2023-01-25 17:01:42 +0700

    tambah article di dashboard

--- app/Http/Controllers/Dashboard/DashboardController.php
+++ app/Http/Controllers/Dashboard/DashboardController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Dashboard;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\User;
+use App\Models\Article;
 use Spatie\Permission\Models\Role;
 
 class DashboardController extends Controller
@@ -20,11 +21,13 @@ class DashboardController extends Controller
         $admin = User::role('Admin')->count();    
         $member = User::role('Member')->count();
         $roles = Role::count();
+        $article = Article::count();
 
         return response()->json([
             'Jumlah Admin Saat Ini' =>  $admin,
             'Jumlah Member Saat Ini' =>  $member,
-            'Jumlah Role Saat Ini' =>  $roles
+            'Jumlah Role Saat Ini' =>  $roles,
+            'Jumlah Article Saat Ini' =>  $article
         ], 200);
     }
 }
